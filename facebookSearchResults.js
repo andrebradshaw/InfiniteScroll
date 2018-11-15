2bcdcf59b80cbfc4b74b8ebf97de1897e9de7903
Andre Bradshaw <bradshaw.andre@gmail.com>
2018-11-15 11:56:58 -0500

    Create facebookSearchResults.js

---
+++ facebookSearchResults.js
@@ -0,0 +1,34 @@
+function runScraper(){
+	var arr = [];
+	var divs = document.getElementsByTagName('div');
+	for(i=0; i<divs.length; i++){
+		var attr = divs[i].getAttribute('data-testid');
+		if(attr == 'browse-result-content'){
+			arr.push(divs[i].parentElement.parentElement.innerText);
+		}
+	}
+	console.log(arr);
+}
+
+function scroller(){
+	window.scrollTo(0, document.body.scrollHeight);
+}
+setTimeout(()=>{
+	scroller();
+},555);
+
+var domObserver = new MutationObserver((mChanges, ob)=>{
+	var endOfScroll = document.getElementById('browse_end_of_results_footer');
+	if(endOfScroll != null){
+		runScraper()
+		ob.disconnect();
+		return;
+	}else{
+		scroller();
+	}
+});
+
+domObserver.observe(document, {
+	childList: true,
+	subtree: true
+});
